a87b6329669b579549ee3b83842571a95b32a854
Patanasak Chunkhajorn <85662524+wateryphoenix@users.noreply.github.com>
2021-07-25 11:29:03 +0700

    Create Exercise5_1_Patanasak_Ch.py

---
+++ Exercise5_1_Patanasak_Ch.py
@@ -0,0 +1,6 @@
+a = int(input("ตัวเลขที่ 1 : "))
+b = int(input("ตัวเลขที่ 2 : "))
+print(a , "+", b, "=", a+b)
+print(a , "-", b, "=", a-b)
+print(a , "*", b, "=", a*b)
+print(a , "/", b, "=", int(a/b))
