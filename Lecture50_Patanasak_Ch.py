a504ce410568a480b1621c7c91c0b6a31c6f3170
Patanasak Chunkhajorn <85662524+wateryphoenix@users.noreply.github.com>
2021-08-01 14:32:03 +0700

    Create Lecture50_Patanasak_Ch.py

---
+++ Lecture50_Patanasak_Ch.py
@@ -0,0 +1,15 @@
+x = int(input("x ="))
+y = int(input("y ="))
+def addNumber(x,y):
+    print(x,"+",y,"=",x+y)
+def subtractNumber(x,y):
+    print(x,"-",y,"=",x-y)
+def multiplyNumber(x,y):
+    print(x,"*",y,"=",x*y)
+def devideNumber(x,y):
+    print(x,"/",y,"=",x/y)
+
+addNumber(x,y)
+subtractNumber(x,y)
+multiplyNumber(x,y)
+devideNumber(x,y)
